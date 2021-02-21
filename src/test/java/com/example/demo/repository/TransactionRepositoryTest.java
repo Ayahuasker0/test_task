4b6bc7f4169d0ca0342593cd8634f138a4ff3316
aklezovics <arturs.klezovics@raisin.com>
2021-02-21 22:37:46 +0100

    Add test descriptions

--- src/test/java/com/example/demo/repository/TransactionRepositoryTest.java
+++ src/test/java/com/example/demo/repository/TransactionRepositoryTest.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//TODO Create tests
+//TODO Create tests https://reflectoring.io/spring-boot-data-jpa-test/
 class TransactionRepositoryTest {
 
 }
