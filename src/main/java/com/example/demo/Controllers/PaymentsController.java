981f01e1078e0ad7fc47ec31e847006cb1df0e51
taras <oldrastabox@gmail.com>
2021-02-21 20:32:14 +0300

    some shit

--- src/main/java/com/example/demo/Controllers/PaymentsController.java
+++ src/main/java/com/example/demo/Controllers/PaymentsController.java
@@ -1,11 +1,29 @@
 package com.example.demo.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.Entity.Transaction;
+import com.example.demo.Entity.TransactionAccount;
+import com.example.demo.Service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
+@RequestMapping("/payment")
 public class PaymentsController {
-    @RequestMapping("/pay")
-    public
+
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentsController(PaymentService paymentService) {this.paymentService = paymentService;}
+
+    @PostMapping("/pay")
+    public void pay(@RequestParam TransactionAccount transactionAccount){
+        PaymentService.pay();
+    }
+
+    @GetMapping("/get_log")
+    public Set<Transaction> getTransactionByAccount(@RequestParam("transactionAccountId") String transactionAccountId){
+        
+    }
 }
